228a0f710128741e4bf307a1c796b459ca4de861
Lenovo <pvk1@nemandi.ntv.is>
2022-04-01 13:21:48 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+ calculateSum(10,20);
+
+ function calculateSum(firstNumber,secondNumber) {
+     const sum = firstNumber + secondNumber;
+     console.log(sum);
+ }
+ 
+
+ calculateDifference(5,10);
+
+  function calculateDifference(firstNumber, secondNumber){ 
+    const sum = firstNumber - secondNumber;
+    console.log(sum);
+  }
+
+addFirstNameToPerson('Pétur')
+
+   function addFirstNameToPerson(firstName){
+       const person = {
+           firstName: firstName,
+       }
+       console.log(person)
+   }
+
+
+
+   calculateModulus(11,2);
+
+ function calculateModulus(firstNumber, secondNumber) {
+     
+    const modulus = (firstNumber % secondNumber);
+    console.log(modulus);
+    
+ }
+
+  evenOrTrue(14);
+
+   function evenOrTrue(number){
+       if(number % 2 === 0){
+        console.log(true);
+       }else{
+           console.log(false);
+       }
+    }
+
+
+  
